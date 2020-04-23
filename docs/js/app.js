5a456fedeea77bc95355bc7e05c99c2a17da880a
info@modevx.com <info@modevx.com>
2020-04-23 08:15:44 -0400

    add comments

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,7 +1,6 @@
 // Variables
 const inputFind = document.getElementById('input-find');
 const btnFind = document.getElementById('btn-find');
-
 const ui = new UI();
 
 // Search button event listener
@@ -9,10 +8,11 @@ btnFind.addEventListener('click', (e) => {
   const userInput = inputFind.value;
   const movieToFind = new Movie(userInput);
 
+
   movieToFind.findMovie()
     .then((responseMovies) => {
-      // console.log('findMovie() results: ', responseMovies);
-      ui.displayResultsMovies(responseMovies);
+      // Display movie title, poster
+      ui.displayMovieResults(responseMovies);
     })
     .catch((err) => console.log(err));  
 })
