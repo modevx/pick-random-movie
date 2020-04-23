1eaad6b152dde1f6bc81f92a982a2118349a63f6
info@modevx.com <info@modevx.com>
2020-04-23 17:48:33 -0400

    add null poster_path conditional

--- docs/js/ui.js
+++ docs/js/ui.js
@@ -14,26 +14,28 @@ class UI {
 
     // Create HTML string of all movie titles to be inserted
     returnedMovies.forEach((movie) => {
-      movieTitles +=
-      `
-        <div class="card mb-3" style="max-width: 540px">
-          <div class="row no-gutters">
-            <div class="col-md-4">
-              <img class="card-img p-2" src="https://image.tmdb.org/t/p/w154${movie.poster_path}" alt="poster">
-            </div>
-            <div class="col-md-8">
-              <div class="card-body">
-                <h5 class="card-title">${movie.title}</h5>
-                <p class="card-text">${movie.overview}</p>
-                <div class="text-center">
-                  <button type="button" class="btn btn-success mx-2">Add Movie!</button>
-                  <button type="button" class="btn btn-danger mx-2">Done</button>
+      if(movie.poster_path !== null) {
+        movieTitles +=
+        `
+          <div class="card mb-3" style="max-width: 540px">
+            <div class="row no-gutters">
+              <div class="col-md-4">
+                <img class="card-img p-2" src="https://image.tmdb.org/t/p/w154${movie.poster_path}" alt="poster">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">${movie.title}</h5>
+                  <p class="card-text">${movie.overview}</p>
+                  <div class="text-center">
+                    <button type="button" class="btn btn-success mx-2">Add Movie!</button>
+                    <button type="button" class="btn btn-danger mx-2">Done</button>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      `
+        `
+      }
     });
 
     this.results.innerHTML = movieTitles;
