a600d5c13817d92668846818b3e7794aa58c8dcd
info@modevx.com <info@modevx.com>
2020-04-24 22:23:38 -0400

    remove log statements and 'done' buttons

--- docs/js/ui.js
+++ docs/js/ui.js
@@ -3,44 +3,39 @@ class UI {
     // NOT USING THESE - BEING CREATED DYNAMICALLY
     // this.title = document.getElementById('title');
     // this.image = document.getElementById('image');
-    this.results = document.getElementById("search-results");
+    this.resultsElement = document.getElementById("search-results");
   }
 
-  displayMovieResults(responseMovies) {
-    console.log("RETURNED MOVIE RESULTS", responseMovies.results);
-    // Step 1. create and display title element for each movie returned
-    let returnedMovies = responseMovies.results;
-    let movieTitles = "";
+  displayMovieResults(returnedMovies) {
+    
+    let displayMovies = "";
 
     // Create HTML string of all movie titles to be inserted
-    returnedMovies.forEach((movie) => {
-      if(movie.poster_path != null) {
-        movieTitles +=
+    returnedMovies.forEach((movie) => {      
+        displayMovies +=
         `
           <div class="card mb-3" style="max-width: 540px">
             <div class="row no-gutters">
               <div class="col-md-4">
-                <img class="card-img p-2" src="https://image.tmdb.org/t/p/w154${movie.poster_path}" alt="poster">
+                <img class="card-img p-2" src="https://image.tmdb.org/t/p/original${movie.poster_path}" alt="poster">
               </div>
               <div class="col-md-8">
                 <div class="card-body">
                   <h5 class="card-title">${movie.title}</h5>
                   <p class="card-text">${movie.overview}</p>
                   <div class="text-center">
-                    <button type="button" class="btn btn-success mx-2">Add Movie!</button>
-                    <button type="button" class="btn btn-danger mx-2">Done</button>
+                    <button id="btn-add"type="button" class="btn btn-success mx-2">Add Movie!</button>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        `
-      }
+        `      
     });
 
-    this.results.innerHTML = movieTitles;
+    this.resultsElement.innerHTML = displayMovies;
 
-    // this.title.textContent = responseMovies.results[0].original_title;
+    // this.title.textContent = returnedMovies.results[0].original_title;
     // this.image.setAttribute('src', `https://image.tmdb.org/t/p/w154${responseMovies.results[0].poster_path}`)
   }
 }
