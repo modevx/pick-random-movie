17ca7e24f9cba91ed31a210160c2e48063a32293
info@modevx.com <info@modevx.com>
2020-04-24 22:23:01 -0400

    add class method skeletons

---
+++ docs/js/storage.js
@@ -0,0 +1,20 @@
+class Storage {
+  constructor() {
+    
+  }
+
+  // Add movie to list
+  addMovie(){
+
+  }
+
+  // Delete movie from list
+  deleteMovie() {
+
+  }
+
+  // Reset _watchList_ once user watches all movies in list
+  resetMovieList() {
+    
+  }
+}
