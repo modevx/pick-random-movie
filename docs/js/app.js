b19ce24a5de1e67b42d6bd5d00e68e236d83ec4f
info@modevx.com <info@modevx.com>
2020-04-24 22:20:38 -0400

    add new array that filters out non-poster results

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,21 +1,35 @@
 // Variables
-const inputFind = document.getElementById('input-find');
-let userInput;
-const btnFind = document.getElementById('btn-find');
+const input_movie = document.getElementById('input-find');
+const btn_search = document.getElementById('btn-find');
+let arr_movies;
+
 const ui = new UI();
+const tmdb = new TMDB();
+const storage = new Storage();
 
 // Search button event listener
-btnFind.addEventListener('click', (e) => {
-  userInput = inputFind.value;
-  const movieToFind = new Movie(userInput);
+btn_search.addEventListener('click', (e) => {
+  // movie user's searching for
+  const usersMovie = input_movie.value;
+  
+  // API call that returns matching titles
+  tmdb.searchMovieTitles(usersMovie)
+  .then((returnedMovies) => {
+    
+    // create new array filtering out movies without images
+      arr_movies = [];
+      returnedMovies.results.forEach(movie => {
+        if(movie.poster_path != null) {
+          arr_movies.push(movie);
+        }
+      });
+      
+      // display results from new filtered array
+      ui.displayMovieResults(arr_movies);      
 
-  movieToFind.findMovie()
-    .then((responseMovies) => {
-      // Display movie title, poster
-      ui.displayMovieResults(responseMovies);
+      console.log('NEW MOVIE ARRAY:', arr_movies);
     })
-    .catch((err) => console.log(err));  
-
-
+    .catch((err) => console.log(err));
 })
+
 
