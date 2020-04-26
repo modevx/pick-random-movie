e64e1afeb0e3f4388485229ef4243e943c30e3be
admin <admin@admins-MacBook-Pro.local>
2020-04-26 14:48:17 -0400

    remove unecessary comments

--- docs/js/tmdb.js
+++ docs/js/tmdb.js
@@ -16,23 +16,15 @@ class TMDB {
       // Return only animated & family movies
       // Filter out 'adult' genre
       try {
-        if(movie.poster_path != null && (movie.genre_ids.includes(16) || movie.genre_ids.includes(10751))) {
+        if(movie.poster_path != null && (movie.genre_ids.includes(16) && movie.genre_ids.includes(10751))) {
           arr_searchResults.push(movie);
-          console.log('searchMovieTitles(): arr_searchResults', arr_searchResults);
         }
       } catch {
         throw 'Error filtering results';
       }
-
-      // if(arr_searchResults.length < 1) {
-      //   console.log('searchMovieTitles(): Search returned no results');
-        // let resultsElement = document.getElementById('search-results');
-        // const noResultsMessage = document.createElement('P').innerText = 'We couldn\'t find any kids movies by that name';
-        // resultsElement.innerHTML = `
-        //   <p>Your search for ${usersMovie} didn't return any results..</p>
-        // `;
-      // }   
     });
+    console.log('searchMovieTitles(): arr_searchResults', arr_searchResults);
+
     return arr_searchResults;      
   }
 }
