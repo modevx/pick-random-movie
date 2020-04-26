8ec369d688d0319709c968bd46b4c70ed2c6d4fa
admin <admin@admins-MacBook-Pro.local>
2020-04-26 00:47:18 -0400

    add returned search array functionality

--- docs/js/tmdb.js
+++ docs/js/tmdb.js
@@ -4,10 +4,22 @@ class TMDB {
   }
 
   async searchMovieTitles(usersMovie) {
+    arr_searchResults = [];
+
     const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${this.API_KEY}&query=${usersMovie}`);
 
-    const resData = await res.json();
+    const returnedMovies = await res.json();
 
-    return resData;
+    returnedMovies.results.forEach(movie => {
+      // *TO-DO: also filter out _adult_ movies and by genre codes 16(animation) or 10751(family)
+      try {
+        if(movie.poster_path != null && (movie.genre_ids.includes(16) || movie.genre_ids.includes(10751))) {
+          arr_searchResults.push(movie);
+        }
+      } catch {
+        throw 'Error filtering results';
+      }
+    });
+    return arr_searchResults;
   }
 }
