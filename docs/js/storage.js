3cbda42699ef5391dfdccc606e68d76eca9ac144
admin <admin@admins-MacBook-Pro.local>
2020-04-26 14:47:54 -0400

    make getUserMovies() static and return movieList

--- docs/js/storage.js
+++ docs/js/storage.js
@@ -1,40 +1,37 @@
 class Storage {
-  constructor() {
-    this.movieList;
-    this.emptyMovieList = [];
-  }
 
   // Get existing movie list to display
-  getUserMovies() {
+  static getUserMovies() {
+    let movieList;
     if(localStorage.getItem('ls_movieList') === null ) {
-      this.movieList = this.emptyMovieList;
+      movieList = [];
     } else {
-      this.movieList = localStorage.getItem('ls_movieList');
+      movieList = JSON.parse(localStorage.getItem('ls_movieList'));
     }
-    console.log('getuserMovies():', this.movieList);
+    console.log('getUserMovies(): current movie list', movieList);
+    return movieList;
   }
 
   // Add movie to list
-  addToUserMovies(thisMovie){
+  static addToUserMovies(thisMovie){
     try {
-      const currentMovieList = this.movieList;
-      console.log('addToUserMovies(): currentMovieList', currentMovieList);
-      // currentMovieList.push(thisMovie);
-      // console.log(currentMovieList);
-      // this.movieList.push(thisMovie);
-      // localStorage.setItem('ls_movieList', this.movieList);
+      let movieList = Storage.getUserMovies();
+
+      movieList.push(thisMovie);
+
+      localStorage.setItem('ls_movieList', JSON.stringify(movieList));
     } catch {
       throw 'Error adding movie to local storage..';
     }
   }
 
   // Delete movie from list
-  deleteFromUserMovies() {
+  static deleteFromUserMovies() {
     // this.userMovies.filter()
   }
 
   // Reset all movies 'watched' property to false
-  resetUserMovies() {
+  static resetUserMovies() {
     // this.userMovies.map()
   }
 }
