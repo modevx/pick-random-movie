85e394b15f32c67a8ce3ca56cee7e6d82bc64f24
admin <admin@admins-MacBook-Pro.local>
2020-04-26 16:48:15 -0400

    comment out log statements

--- docs/js/tmdb.js
+++ docs/js/tmdb.js
@@ -4,13 +4,13 @@ class TMDB {
   }
 
   async searchMovieTitles(usersMovie) {
-    console.log('searchMovieTitles(): search button clicked');
+    // console.log('searchMovieTitles(): search button clicked');
     arr_searchResults = [];
 
     const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${this.API_KEY}&query=${usersMovie}`);
 
     const returnedMovies = await res.json();
-    console.log('searchMovieTitles(): returnedMovies', returnedMovies);
+    // console.log('searchMovieTitles(): search results', returnedMovies);
 
     returnedMovies.results.forEach(movie => {
       // Return only animated & family movies
@@ -23,7 +23,7 @@ class TMDB {
         throw 'Error filtering results';
       }
     });
-    console.log('searchMovieTitles(): arr_searchResults', arr_searchResults);
+    // console.log('searchMovieTitles(): filtered results', arr_searchResults);
 
     return arr_searchResults;      
   }
