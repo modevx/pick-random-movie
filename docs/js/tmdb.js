97b182020b188093594c5782de715fee15c9c068
info@modevx.com <info@modevx.com>
2020-04-27 00:10:56 -0400

    clean up logging and comments

--- docs/js/tmdb.js
+++ docs/js/tmdb.js
@@ -3,27 +3,28 @@ class TMDB {
     this.API_KEY = '8138a9c0b29932d322390d4e16afab9f';
   }
 
-  async searchMovieTitles(usersMovie) {
-    // console.log('searchMovieTitles(): search button clicked');
-    arr_searchResults = [];
-
-    const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${this.API_KEY}&query=${usersMovie}`);
-
+  async searchMovieTitles(movieToFind) {
+    console.log('MOVIES IN LOCAL STORAGE:', Storage.getUserMovies());
+    // API fetch request
+    const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${this.API_KEY}&query=${movieToFind}`);
+    // full API response
     const returnedMovies = await res.json();
-    // console.log('searchMovieTitles(): search results', returnedMovies);
+    // array used to hold newly created Movie objects
+    let arr_searchResults = [];
 
+    // Return only animated & family movies
+    // Filter out 'adult' genre
+    // Create new Movie objects and add to return array
     returnedMovies.results.forEach(movie => {
-      // Return only animated & family movies
-      // Filter out 'adult' genre
       try {
         if(movie.poster_path != null && (movie.genre_ids.includes(16) && movie.genre_ids.includes(10751))) {
-          arr_searchResults.push(movie);
+          const newMovie = new Movie(movie);
+          arr_searchResults.push(newMovie);
         }
       } catch {
         throw 'Error filtering results';
       }
     });
-    // console.log('searchMovieTitles(): filtered results', arr_searchResults);
 
     return arr_searchResults;      
   }
