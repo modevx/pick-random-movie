98665b7a085d0e4774fe75285909f69c08022e13
info@modevx.com <info@modevx.com>
2020-04-27 00:10:32 -0400

    complete adding to local storage functionality

--- docs/js/storage.js
+++ docs/js/storage.js
@@ -9,41 +9,26 @@ class Storage {
     } else {
       movieList = JSON.parse(localStorage.getItem('ls_movieList'));
     }
-    // console.log('getUserMovies(): current movie list', movieList);
 
     return movieList;
   }
 
   // Add movie to list
   static addToUserMovies(thisMovie){
-    let movieList = Storage.getUserMovies();
-    const movieID = thisMovie.id;
-    // console.log('THIS MOVIE ID:', movieID);
+    // retrieve current userMovieList from local storage
+    let movieList = Storage.getUserMovies();    
 
     try {
+      // test if movie is already in users movie list using sort()
+      let hasMovie = (movie) => movie.id === thisMovie.id;
+      // if sort()n returns false (movie not currently in list) -> add movie
+      if (!movieList.some(hasMovie)) {
         movieList.push(thisMovie);
         localStorage.setItem('ls_movieList', JSON.stringify(movieList));
-      
-        console.log('LIST AFTER ADDING:', JSON.parse(localStorage.getItem('ls_movieList')));
-      // console.log('MOVIE LIST FROM LOCAL STORGAGE:', movieList);
-      // if list is empty add new movie
-      // if(movieList === null) {
-      //   movieList.push(thisMovie);
-      //   localStorage.setItem('ls_movieList', JSON.stringify(movieList));
-      // verify movie to be added doesnt already exist in current movie list
-      // else {
-      //   for(let i = 0; i < movieList.length; i++) {
-      //     let ids = [];
-      //     ids.push(movieList[i].id);
-      //   }
-      //   if(!ids.includes(movieID)) {
-      //     movieList.push(thisMovie);
-      //   }
-      //   console.log('CURRENT MOVIE IDS ARRAY', ids);
-      // }
-      
-      // console.log('FILTERED ID LIST', ids);      
-
+        // *TO-DO: create UI method alertMovieAdded() - 'Movie added!'
+      } else {
+        // *TO-DO: create UI method alertAlreadyAdded() - 'This movie's already in your list!'
+      }      
     } catch {
       throw 'Error adding movie to local storage..';
     }
