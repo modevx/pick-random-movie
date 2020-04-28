3f42a60a5d6fd6684923d0841388ee63670a2838
info@modevx.com <info@modevx.com>
2020-04-28 00:35:10 -0400

    add delete functionality

--- docs/js/storage.js
+++ docs/js/storage.js
@@ -4,17 +4,17 @@ class Storage {
   static getUserMovies() {
     let movieList;
 
-    if(localStorage.getItem('ls_movieList') === null ) {
+    if(localStorage.getItem('ls_movieList') === null) {
       movieList = [];
     } else {
       movieList = JSON.parse(localStorage.getItem('ls_movieList'));
     }
-    console.log('getUserMovies() - movies in local storage:', movieList);
     return movieList;
   }
 
   // Add movie to list
   static addToUserMovies(thisMovie){
+    console.log('--- addToUserMovies() ---');
     // retrieve current userMovieList from local storage
     let movieList = Storage.getUserMovies();    
 
@@ -25,7 +25,6 @@ class Storage {
       if (!movieList.some(hasMovie)) {
         movieList.push(thisMovie);
         localStorage.setItem('ls_movieList', JSON.stringify(movieList));
-        console.log('addToUserMovies() - movie added!', JSON.parse(localStorage.getItem('ls_movieList')));
         // *TO-DO: create UI method alertMovieAdded() - 'Movie added!'
       } else {
         // *TO-DO: create UI method alertAlreadyAdded() - 'This movie's already in your list!'
@@ -36,8 +35,22 @@ class Storage {
   }
 
   // Delete movie from list
-  static deleteFromUserMovies() {
-    // this.userMovies.filter()
+  static deleteFromUserMovies(thisMovie) {
+    console.log('--- deleteFromUserMovies() ---');
+    // retrieve current userMovieList from local storage
+    let movieList = Storage.getUserMovies();
+    // if last movie in list, remove key from storage
+    if(movieList.length == 1) {
+      localStorage.removeItem('ls_movieList');
+      UI.displayEditMoviesScreen();
+    } else {   
+      // if movie list has multiple movies, remove movie from movieList
+      movieList = movieList.filter((movie) => movie.id !== thisMovie.id);
+      // update local storage with new movieList
+      localStorage.setItem('ls_movieList', JSON.stringify(movieList));
+      // display updated movieList
+      UI.displayEditMoviesScreen();
+    }
   }
 
   // Reset all movies 'watched' property to false
