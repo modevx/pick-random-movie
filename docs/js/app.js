9e2c9eefee7d2c017d313b4152b02331d80354b3
info@modevx.com <info@modevx.com>
2020-04-27 20:55:30 -0400

    add initial eventListeners

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,4 +1,5 @@
 // Variables
+const home = document.getElementById('nav-link-home')
 const watchMovie = document.getElementById('nav-link-watch');
 const editMovies = document.getElementById('nav-link-edit');
 const input_movie = document.getElementById('input-find');
@@ -11,22 +12,17 @@ let arr_searchResults;
 // Get user's current movie list
 document.addEventListener('DOMContentLoaded', Storage.getUserMovies);
 
-// Search button event listener
-btn_search.addEventListener('click', (e) => {
-  // create variable that holds user's search input
-  // make API call that returns matching titles
-  // display results 
-  // create btn-add elements
-  // clear search field
-  const movieToFind = input_movie.value;
-  
-  tmdb.searchMovieTitles(movieToFind)
-  .then((arr_searchResults) => {    
-    UI.displayMovieResults(arr_searchResults); 
-  })
-  .catch((err) => console.log(err));
+// ADD EVENT LISTENERS TO UI ELEMENTS
+// Home nav link
+home.addEventListener('click', UI.displayHomeScreen);
 
-  input_movie.value = '';
-});
+// Watch A Movie nav link
+watchMovie.addEventListener('click', UI.displayMovieToWatch);
+
+// Edit My Movies nav link
+editMovies.addEventListener('click', UI.displayEditMoviesScreen);
+
+// Find Movie button
+btn_search.addEventListener('click', TMDB.findNewMovie);
 
 
