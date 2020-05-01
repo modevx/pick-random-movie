f4385c38916baaab6a8a5de698404de6824d35c0
info@modevx.com <info@modevx.com>
2020-04-30 23:05:59 -0400

    add renderWatchAMovie initial logic

--- docs/js/ui.js
+++ docs/js/ui.js
@@ -9,11 +9,15 @@ class UI {
   static displayWatchAMovieScreen() {
     console.log('UI.displayWatchAMovieScreen()');
     const userMovies = Storage.getUserMovies();
+    const unwatchedMovies = userMovies.filter((movie) => movie.watched === false);
+
+    console.log('UNWATCHED MOVIES:', unwatchedMovies);
     
-    if(userMovies.length < 1) {
+    if(unwatchedMovies.length < 1) {
       UI.alertNoMoviesInList();
     } else {
       // render MovieRoulette Logo and PICK A MOVIE button
+      UI.renderWatchAMovie(unwatchedMovies);
     }
   }  
   
@@ -93,7 +97,7 @@ class UI {
     }
   }
 
-  // ----- RENDER MOVIES TO UI ----- 
+  // ----- RENDER ELEMENTS TO UI ----- 
 static renderAddMovieCards(arr_searchResults) {
     console.log('UI.renderAddMovieCards()');
     let outputHTML = '';
@@ -148,7 +152,48 @@ static renderAddMovieCards(arr_searchResults) {
       UI.renderDivHTML(outputHTML);
   }
 
+  static renderWatchAMovie(unwatchedMovies) {
+    console.log('renderWatchAMovie()');
+    let outputHTML = `
+      <div>
+        <i class="fas fa-film fa-10x" style="color: orange"></i>
+      </div>
+      <button type="button" class="btn btn-pick btn-outline-success btn-lg">Movie Roulette!</button>
+    `;
+    UI.renderDivHTML(outputHTML);
+
+    const btn_pickMovie = document.getElementsByClassName('btn-pick');
+
+    btn_pickMovie[0].addEventListener('click', (e) => {
+      Storage.pickRandomMovie(unwatchedMovies);
+    });
+  }
+
+  static renderRandomMovie(randomMovie) {
+    console.log('renderRandomMovie()');
+    let outputHTML = `
+        <div class="card mb-3" style="max-width: 540px">
+          <div class="row no-gutters">
+            <div class="col-md-4">
+              <img class="card-img p-2" src="https://image.tmdb.org/t/p/original${randomMovie.poster_path}" alt="poster">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">${randomMovie.title}</h5>
+                <p class="card-text">${randomMovie.overview}</p>
+                <div class="text-center">
+                  <button type="button" class="btn btn-add btn-success mx-2">Add To My List!</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        `;
+      UI.renderDivHTML(outputHTML);      
+  }
+
   static renderDivHTML(html) {
+    console.log('renderDivHTML()');
     const displayDiv = document.getElementById('render');
     displayDiv.innerHTML = html;
     displayDiv.style.display = 'block';
@@ -157,6 +202,7 @@ static renderAddMovieCards(arr_searchResults) {
   // ----- ALERT MESSAGES ----- 
   
   static alertNoMoviesInList() {
+    console.log('alertNoMoviesInList()');
     const addMoviesMessage = `You haven't added any movies to watch!`;
     const displayDiv = document.getElementById('render');
     displayDiv.style.display = 'block';
