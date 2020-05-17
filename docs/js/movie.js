c9950bfd1ce6dc7ca9cea23a6046199e6a4445af
Ephraim <ephraimjsmith@gmail.com>
2020-05-17 06:30:52 -0400

    add watched( )

--- docs/js/movie.js
+++ docs/js/movie.js
@@ -1,14 +1,13 @@
 class Movie {
   constructor(newMovie) {
-    this.id = newMovie.id;  
+    this.id = newMovie.id;
     this.poster_path = newMovie.poster_path;
     this.title = newMovie.original_title;
-    this.overview = newMovie.overview;     
-    this.watched = false;      
+    this.overview = newMovie.overview;
+    this.watched = false;
   }
 
   watched() {
-    // this.watched = true;
+    this.watched = true;
   }
-
 }
